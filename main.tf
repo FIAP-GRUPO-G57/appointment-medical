8d5b16b14cb8b1e370d91ff552df347e4f858ec8
alexandresampaioo <alexandresampaio@gmail.com>
2024-07-27 14:28:27 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_lambda_function" "app" {
 resource "aws_dynamodb_table" "appointments" {
   name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
+  hash_key     =  "id"
 
   attribute {
     name = "id"
